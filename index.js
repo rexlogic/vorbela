3971ff838be4e4750e81df6cde60027cbb6ed3cc
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-06 21:53:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const axios = require('axios')
 const app = express()
-
-var location = 'Slatina'
-
-const getWeather = location => {
-  const params = {
-    q: location,
-    APPID: process.env.OWM_APIK,
-    units: 'metric',
-    lang: 'ro'
-  }
-
-  return axios.get('https://api.openweathermap.org/data/2.5/weather', { params })
-    .then(({ data }) => data)
-}
 
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
@@ -26,12 +11,11 @@ app.get('/', function (req, res) {
 
 app.post('/', (req, res) => {
   console.log(req.body.nlp.source)
-  console.log(getWeather(location))
   
   res.send({
     replies: [{
       type: 'text',
-      content: 'Vremea în ' + location + ': ' + getWeather(location),
+      content: 'Vremea în ' + ': ',
     }], 
     conversation: {
       memory: { key: 'value' }
