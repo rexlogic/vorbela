98958265914f8fb6cfe227fb52555d58e2344e6b
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-06 20:49:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/', (req, res) => {
   res.send({
     replies: [{
       type: 'text',
-      content: 'Vremea în ' + location + ':' + getWeather,
+      content: 'Vremea în ' + location + ': ' + getWeather.data,
     }], 
     conversation: {
       memory: { key: 'value' }
