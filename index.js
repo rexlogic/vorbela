b9f8c2ce9d2c53b3de4ee1663826e6615aafc175
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-06 20:30:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
-const app = express() 
-const port = 5000
+const app = express()
 
 const getWeather = (location) => {
   const params = {
     q: location,
-    APPID: config.WEATHER_TOKEN,
+    APPID: process.env.OWM_APIK,
     units: 'metric',
   }
 
