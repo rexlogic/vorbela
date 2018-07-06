30ea045f6da0d08e06f3868007f1f16b865e0757
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-06 20:21:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const app = express() 
 const port = 5000
+
+const getWeather = (location) => {
+  const params = {
+    q: location,
+    APPID: config.WEATHER_TOKEN,
+    units: 'metric',
+  }
+
+  return axios.get('https://api.openweathermap.org/data/2.5/weather', { params })
+    .then(({ data }) => data)
+}
 
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
