0903433355fdef4783bef2d28608f01e789572a4
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-07 16:19:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const OpenWeatherMapHelper = require('openweathermap-node')
 const app = express()
-const helper = new OpenWeatherMapHelper(
-    {
-        APPID: process.env.OWM_APIK,
-        units: 'metric'
-    }
-)
 
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 
-helper.getCurrentWeatherByCityName('Slatina', (err, currentWeather) => {
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log(currentWeather)
-    }
-})
-
 app.get('/', function (req, res) {
-  res.send('vremea')
+  res.send('<html><body><div>vremea</div></body></html>')
 })
 
 app.post('/', (req, res) => {
