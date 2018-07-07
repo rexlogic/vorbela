45a11d86d0d36f1e29e78891d6878b1f1f211920
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-07 22:00:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 
 app.post('/', (req, res) => {
   console.log(req.body)
+  console.log(req.body.nlp.entities.location_ro[0])
   let city = 'Slatina'
   let apiKey = process.env.OWM_APIK
   let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city + '&units=metric&appid=' + apiKey
