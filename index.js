03e7f67ce6bace94993941651fa09a826fd55b5f
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-07 16:22:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-  res.send('<html><body><div>vremea</div></body></html>')
+  res.send('<!DOCTYPE html><html><body><div>vremea</div></body></html>')
 })
 
 app.post('/', (req, res) => {
