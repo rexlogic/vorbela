4ca25cb8d86ae76d644ab945d9a95a2c2ed6b765
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-07-09 21:55:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const date = require('date-and-time')
 const app = express()
 
 app.use(bodyParser.json())
