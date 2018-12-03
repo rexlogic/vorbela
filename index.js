3fd8e904f16a006957fed9487e6531c1222aa720
rexlogic <40705463+rexlogic@users.noreply.github.com>
2018-12-03 23:14:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -185,6 +185,8 @@ app.post('/stiri', (req, res) => {
         })
       } else {
         let stiriText = []
+        console.log('st.totalResults=' + st.totalResults)
+        console.log('st.articles[0].source.name=' + st.articles[0].source.name)
         for (i=0; i < st.totalResults; i++) {
           stiriText.push({
             title: st.articles[i].source.name,
